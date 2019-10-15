92bd95ca40ca7ccbab1e671c9dfed2c10b442a29
Nathalia Lima <95.nathallya@gmail.com>
2019-10-15 18:18:59 -0300

    removendo ca ga da

--- src/services/apollo.js
+++ src/services/apollo.js
@@ -2,7 +2,7 @@ import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache, ApolloLink, HttpLink } from "apollo-boost";
 
-const httpLink = new HttpLink({ uri: 'https://daily-diet-node.herokuapp.com' });
+const httpLink = new HttpLink({ uri: '' });
 
 const authLink = new ApolloLink((operation, forward) => {
     const token = "";
